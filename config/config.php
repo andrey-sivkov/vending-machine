6d6053b6b82246646b5bc7d6867ffa006078b5d6
Andrey Sivkov <sadko@yandex.ru>
2021-10-18 23:07:40 +0300

    Config

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+// параметры соединения с базой данных
+const DB_SERVER = 'localhost'; // адрес сервера MySQL
+const DB_SERVER_USERNAME = 'username'; // имя пользователя БД
+const DB_SERVER_PASSWORD = 'password'; // пароль пользователя
+const DB_DATABASE = 'database'; // имя базы данных
